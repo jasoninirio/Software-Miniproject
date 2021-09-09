d6cbd338040d716a06afcaa4f2c66a1adff12ca7
Jasoninirio <jasonini@bu.edu>
2021-09-09 17:54:14 -0400

    attempting ios build

--- healthi_app/lib/main.dart
+++ healthi_app/lib/main.dart
@@ -1,15 +1,165 @@
-// import 'dart:js'
+// import 'dart:js';
+import 'dart:async';
+import 'dart:convert' show json;
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Firebase Core
+import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore Database
+import 'package:firebase_auth/firebase_auth.dart'; // Firebase Auth (Might not be needed)
+import 'package:google_sign_in/google_sign_in.dart'; // Google Sign-In
+import "package:http/http.dart" as http;
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
 
 void main() {
+  Firebase.initializeApp();
   runApp(MaterialApp(
+    home: SignInPage(),
     initialRoute: '/',
     routes: {
       '/': (context) => HomePage(),
       '/camera': (context) => CameraPage(),
       '/profile': (context) => ProfilePage(),
     },
+    title: "Healthi App",
   ));
+}
+
+// Login/Sign-in Page - gets user's information from firestore
+class SignInPage extends StatefulWidget {
+  @override
+  State createState() => SignInState();
+}
+
+class SignInState extends State<SignInPage> {
+  GoogleSignInAccount? _currentUser;
+  String _contactText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        _handleGetContact(_currentUser!);
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Future<void> _handleGetContact(GoogleSignInAccount user) async {
+    setState(() {
+      _contactText = "Loading contact info...";
+    });
+    final http.Response response = await http.get(
+      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
+          '?requestMask.includeField=person.names'),
+      headers: await user.authHeaders,
+    );
+    if (response.statusCode != 200) {
+      setState(() {
+        _contactText = "People API gave a ${response.statusCode} "
+            "response. Check logs for details.";
+      });
+      print('People API ${response.statusCode} response: ${response.body}');
+      return;
+    }
+    final Map<String, dynamic> data = json.decode(response.body);
+    final String? namedContact = _pickFirstNamedContact(data);
+    setState(() {
+      if (namedContact != null) {
+        _contactText = "I see you know $namedContact!";
+      } else {
+        _contactText = "No contacts to display.";
+      }
+    });
+  }
+
+  String? _pickFirstNamedContact(Map<String, dynamic> data) {
+    final List<dynamic>? connections = data['connections'];
+    final Map<String, dynamic>? contact = connections?.firstWhere(
+      (dynamic contact) => contact['names'] != null,
+      orElse: () => null,
+    );
+    if (contact != null) {
+      final Map<String, dynamic>? name = contact['names'].firstWhere(
+        (dynamic name) => name['displayName'] != null,
+        orElse: () => null,
+      );
+      if (name != null) {
+        return name['displayName'];
+      }
+    }
+    return null;
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
+  Widget _buildBody() {
+    GoogleSignInAccount? user = _currentUser;
+    if (user != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: user,
+            ),
+            title: Text(user.displayName ?? ''),
+            subtitle: Text(user.email),
+          ),
+          const Text("Signed in successfully."),
+          Text(_contactText),
+          ElevatedButton(
+            child: const Text('SIGN OUT'),
+            onPressed: _handleSignOut,
+          ),
+          ElevatedButton(
+            child: const Text('REFRESH'),
+            onPressed: () => _handleGetContact(user),
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const Text("You are not currently signed in."),
+          ElevatedButton(
+            child: const Text('SIGN IN'),
+            onPressed: _handleSignIn,
+          ),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Google Sign In'),
+        ),
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: _buildBody(),
+        ));
+  }
 }
 
 // Home Page - hosts some neat things like a calorie tracker (?)
@@ -20,7 +170,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('healthi'),
         centerTitle: true,
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.green,
       ),
       body: Center(
         child: Column(
