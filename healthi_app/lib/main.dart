69d471ac036324776e0cd38b86ff3b45fcd94f5d
jasoninirio <jasonini@bu.edu>
2021-09-10 17:38:32 -0400

    finished login prototype

--- healthi_app/lib/main.dart
+++ healthi_app/lib/main.dart
@@ -1,248 +1,33 @@
-// import 'dart:js';
-// --no-sound-null-safety
-import 'dart:async';
-import 'dart:convert' show json;
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // Firebase Core
-import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore Database
-import 'package:firebase_auth/firebase_auth.dart'; // Firebase Auth (Might not be needed)
-import 'package:google_sign_in/google_sign_in.dart'; // Google Sign-In
-import "package:http/http.dart" as http;
+import 'package:healthi_app/login.dart';
 
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: [
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
-);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  Firebase.initializeApp();
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => SignInPage(),
-      '/home': (context) => HomePage(),
-      '/camera': (context) => CameraPage(),
-      '/profile': (context) => ProfilePage(),
-    },
-    title: "Healthi App",
-  ));
+  // init firebase
+  await Firebase.initializeApp();
+
+  // init app
+  runApp(SignIn());
 }
 
-// Login/Sign-in Page - gets user's information from firestore
-class SignInPage extends StatefulWidget {
+class SignIn extends StatefulWidget {
+  // SignIn({key? key}) : super(key: key);
   @override
-  State createState() => SignInState();
+  _SignIn_state createState() => _SignIn_state();
 }
 
-class SignInState extends State<SignInPage> {
-  GoogleSignInAccount? _currentUser;
-  String _contactText = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      setState(() {
-        _currentUser = account;
-      });
-      if (_currentUser != null) {
-        _handleGetContact(_currentUser!);
-      }
-    });
-    _googleSignIn.signInSilently();
-  }
-
-  Future<void> _handleGetContact(GoogleSignInAccount user) async {
-    setState(() {
-      _contactText = "Loading contact info...";
-    });
-    final http.Response response = await http.get(
-      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
-          '?requestMask.includeField=person.names'),
-      headers: await user.authHeaders,
-    );
-    if (response.statusCode != 200) {
-      setState(() {
-        _contactText = "People API gave a ${response.statusCode} "
-            "response. Check logs for details.";
-      });
-      print('People API ${response.statusCode} response: ${response.body}');
-      return;
-    }
-    final Map<String, dynamic> data = json.decode(response.body);
-    final String? namedContact = _pickFirstNamedContact(data);
-    setState(() {
-      if (namedContact != null) {
-        _contactText = "I see you know $namedContact!";
-      } else {
-        _contactText = "No contacts to display.";
-      }
-    });
-  }
-
-  String? _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic>? connections = data['connections'];
-    final Map<String, dynamic>? contact = connections?.firstWhere(
-      (dynamic contact) => contact['names'] != null,
-      orElse: () => null,
-    );
-    if (contact != null) {
-      final Map<String, dynamic>? name = contact['names'].firstWhere(
-        (dynamic name) => name['displayName'] != null,
-        orElse: () => null,
-      );
-      if (name != null) {
-        return name['displayName'];
-      }
-    }
-    return null;
-  }
-
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-      Navigator.pushNamed(context, '/home');
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  Future<void> _handleSignOut() => _googleSignIn.disconnect();
-
-  Widget _buildBody() {
-    GoogleSignInAccount? user = _currentUser;
-    if (user != null) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: user,
-            ),
-            title: Text(user.displayName ?? ''),
-            subtitle: Text(user.email),
-          ),
-          const Text("Signed in successfully."),
-          Text(_contactText),
-          ElevatedButton(
-            child: const Text('SIGN OUT'),
-            onPressed: _handleSignOut,
-          ),
-          ElevatedButton(
-            child: const Text('REFRESH'),
-            onPressed: () => _handleGetContact(user),
-          ),
-        ],
-      );
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          const Text("You are not currently signed in."),
-          ElevatedButton(
-            child: const Text('SIGN IN'),
-            onPressed: _handleSignIn,
-          ),
-        ],
-      );
-    }
-  }
-
+class _SignIn_state extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Sign In'),
-        ),
-        body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: _buildBody(),
-        ));
-  }
-}
-
-// Home Page - hosts some neat things like a calorie tracker (?)
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('healthi'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
+    return MaterialApp(
+      title: 'Login Screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            // Camera Button
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/camera');
-                },
-                child: Text('Camera')),
-
-            // Profile Button
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/profile');
-                },
-                child: Text('Profile')),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Camera Page - hosts the camera and a search bar to add food items to User's food tracker
-class CameraPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('camera'),
-        backgroundColor: Colors.orange,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            // Back Button
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                },
-                child: Text('Go Back')),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Profile Page - Shows first and last name, recipes, and history stats of User's food items (?)
-class ProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('profile'),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            // Back Button
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                },
-                child: Text('Go Back')),
-          ],
-        ),
-      ),
+      home: LoginScreen(),
     );
   }
 }
