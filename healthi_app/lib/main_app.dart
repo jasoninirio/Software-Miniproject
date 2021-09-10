f10571618307ecbf40d12f7010949bd371c24be6
jasoninirio <jasonini@bu.edu>
2021-09-10 18:40:52 -0400

    adding homepage as stateful

--- healthi_app/lib/main_app.dart
+++ healthi_app/lib/main_app.dart
@@ -2,34 +2,52 @@
 
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('healthi'),
-        centerTitle: true,
         backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            // Camera Button
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/camera');
-                },
-                child: Text('Camera')),
-
-            // Profile Button
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/profile');
-                },
-                child: Text('Profile')),
-          ],
-        ),
+        centerTitle: true,
+        title: Text("Healthi - Home Page"),
       ),
     );
   }
 }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('healthi'),
+//         centerTitle: true,
+//         backgroundColor: Colors.green,
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: <Widget>[
+//             // Camera Button
+//             ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.pushNamed(context, '/camera');
+//                 },
+//                 child: Text('Camera')),
+
+//             // Profile Button
+//             ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.pushNamed(context, '/profile');
+//                 },
+//                 child: Text('Profile')),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
