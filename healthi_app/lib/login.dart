bb6ee5b23bd9d782082adfa11ae0bca8d2f28297
jasoninirio <jasonini@bu.edu>
2021-09-14 02:37:57 -0400

    added cloud firestore support to app

--- healthi_app/lib/login.dart
+++ healthi_app/lib/login.dart
@@ -1,9 +1,22 @@
 //SignInScreen
 
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:healthi_app/main_app.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Connecting to Cloud Firestore
+final firestoreInstance = FirebaseFirestore.instance;
+
+// User Credentials for App
+class userLogin {
+  static String firstName = '';
+  static String lastName = '';
+  static String idToken = '';
+}
 
 class LoginScreen extends StatefulWidget {
 // LoginScreen({Key key}) : super(key: key);
@@ -67,7 +80,20 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-// creating firebase instance
+  static Map<String, dynamic> parseJwt(String token) {
+    // validate token
+    final List<String> parts = token.split('.');
+
+    // retrieve token payload
+    final String payload = parts[1];
+    final String normalized = base64Url.normalize(payload);
+    final String resp = utf8.decode(base64Url.decode(normalized));
+    // convert to Map
+    final payloadMap = json.decode(resp);
+    return payloadMap;
+  }
+
+  // creating firebase instance
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<void> signup(BuildContext context) async {
@@ -77,15 +103,39 @@ class _LoginScreenState extends State<LoginScreen> {
     if (googleSignInAccount != null) {
       final GoogleSignInAuthentication googleSignInAuthentication =
           await googleSignInAccount.authentication;
+      final idToken = googleSignInAuthentication.idToken;
       final AuthCredential authCredential = GoogleAuthProvider.credential(
           idToken: googleSignInAuthentication.idToken,
           accessToken: googleSignInAuthentication.accessToken);
 
-      // Getting users credential
+      // Saving user credentials to collection
       UserCredential result = await auth.signInWithCredential(authCredential);
       User? user = result.user;
 
+      // Get Credentials from login
+      Map<String, dynamic> idMap = parseJwt(idToken!);
+
+      userLogin.firstName = idMap["given_name"];
+      userLogin.lastName = idMap["family_name"];
+      userLogin.idToken = idToken;
+
+      // Add to Cloud FireStore
       if (result != null) {
+        firestoreInstance.collection('Users').doc(user!.uid).set({
+          "Name": "${userLogin.firstName} ${userLogin.lastName}",
+          "Recipes": [],
+        }).then((_) {
+          print("Success!");
+        });
+
+        // Get Credentials from login
+        Map<String, dynamic> idMap = parseJwt(idToken!);
+
+        userLogin.firstName = idMap["given_name"];
+        userLogin.lastName = idMap["family_name"];
+        userLogin.idToken = idToken;
+
+        // Init app
         Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => App_PageView()));
       } // if result not null we simply call the MaterialpageRoute,
--- healthi_app/lib/main_app.dart
+++ healthi_app/lib/main_app.dart
@@ -1,19 +1,39 @@
 // Home page screen
-
+// import 'dart:js'
+// import 'dart:html';
+import 'dart:async';
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'package:healthi_app/login.dart';
 
-// final controller = PageController(
-//   initialPage: 1,
-// );
+// Connecting to Cloud Firestore for creating recipes, adding ingredients, etc.
+final firestoreInstance = FirebaseFirestore.instance;
 
-// final pageView = PageView(
-//   controller: controller,
-//   children: [
-//     CameraPage(),
-//     HomePage(),
-//     ProfilePage(),
-//   ],
-// );
+//Class for the relevant info extracted from the FDC rest API
+class FoodInfo {
+  final String gtinUpc;
+  final int fdcId;
+  final String description;
+  final num calories;
+
+  FoodInfo(
+      {required this.gtinUpc,
+      required this.fdcId,
+      required this.description,
+      required this.calories});
+
+  factory FoodInfo.fromJson(Map<String, dynamic> json) {
+    return FoodInfo(
+        gtinUpc: json['foods'][0]['gtinUpc'],
+        fdcId: json['foods'][0]['fdcId'],
+        description: json['foods'][0]['description'],
+        calories: json['foods'][0]['foodNutrients'][3]['value']);
+  }
+}
 
 class App_PageView extends StatefulWidget {
   @override
@@ -42,24 +62,6 @@ class _App_PageViewState extends State<App_PageView> {
   }
 }
 
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.green,
-//         centerTitle: true,
-//         title: Text("Healthi"),
-//       ),
-//     );
-//   }
-// }
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -72,19 +74,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            // Camera Button
-            // ElevatedButton(
-            //     onPressed: () {
-            //       Navigator.pushNamed(context, '/camera');
-            //     },
-            //     child: Text('Camera')),
-
-            // // Profile Button
-            // ElevatedButton(
-            //     onPressed: () {
-            //       Navigator.pushNamed(context, '/profile');
-            //     },
-            //     child: Text('Profile')),
+            Text("this is the homepage"),
           ],
         ),
       ),
@@ -104,19 +94,7 @@ class ProfilePage extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            // Camera Button
-            // ElevatedButton(
-            //     onPressed: () {
-            //       Navigator.pushNamed(context, '/camera');
-            //     },
-            //     child: Text('Camera')),
-
-            // // Profile Button
-            // ElevatedButton(
-            //     onPressed: () {
-            //       Navigator.pushNamed(context, '/profile');
-            //     },
-            //     child: Text('Profile')),
+            Text("${userLogin.firstName} ${userLogin.lastName}"),
           ],
         ),
       ),
@@ -124,34 +102,129 @@ class ProfilePage extends StatelessWidget {
   }
 }
 
-class CameraPage extends StatelessWidget {
+class CameraPage extends StatefulWidget {
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  String? scanResult = null;
+  late Future<FoodInfo> futureFoodInfo;
+  String myCode = '035107001247';
+
+  @override
+  void initState() {
+    super.initState();
+
+    // futureFoodInfo = fetchFoodInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('camera'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
+          title: Text('Scan a Food Item!'),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent[700]),
+      backgroundColor: Colors.lime[100],
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Camera Button
-            // ElevatedButton(
-            //     onPressed: () {
-            //       Navigator.pushNamed(context, '/camera');
-            //     },
-            //     child: Text('Camera')),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.greenAccent[700], onPrimary: Colors.black),
+              icon: Icon(Icons.camera_alt_outlined),
+              label: Text('Start Scan', style: TextStyle(fontSize: 25)),
+              onPressed: scanBarcode,
+            ),
+            SizedBox(height: 20),
+            Text(
+              scanResult == null
+                  ? 'Scan a barcode first!'
+                  : 'You scanned the barcode: $scanResult',
+              style: TextStyle(fontSize: 23),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 60),
+            Center(
+              child: FutureBuilder<FoodInfo>(
+                future: futureFoodInfo = fetchFoodInfo(scanResult.toString()),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && scanResult != null) {
+                    return Column(children: <Widget>[
+                      Text(
+                        'Food Item Name: ' +
+                            (snapshot.data!.description).toString(),
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Barcode number: ' +
+                            (snapshot.data!.gtinUpc).toString(),
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'FDC ID Number: ' + (snapshot.data!.fdcId).toString(),
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Calories: ' +
+                            (snapshot.data!.calories).toString() +
+                            ' kcal',
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      )
+                    ]);
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
 
-            // // Profile Button
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ),
+            // //Back Button
             // ElevatedButton(
             //     onPressed: () {
-            //       Navigator.pushNamed(context, '/profile');
+            //       scanResult = null;
             //     },
-            //     child: Text('Profile')),
+            //     child: Text('Scan new item')),
           ],
         ),
       ),
     );
+  }
+
+  Future scanBarcode() async {
+    String scanResult;
+
+    try {
+      scanResult = await FlutterBarcodeScanner.scanBarcode(
+          "#6CEE5D", "Cancel", true, ScanMode.BARCODE);
+    } on PlatformException {
+      scanResult = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() => this.scanResult = scanResult);
+  }
+
+  Future<FoodInfo> fetchFoodInfo(String barcode) async {
+    final response = await http.get(Uri.parse(
+        'https://api.nal.usda.gov/fdc/v1/foods/search?&api_key=K5TALOAH4AWhPCKKbZsz1gIzRgnNuaC2OPB1lhsR&query=$barcode&dataType=Branded'));
+
+    if (response.statusCode == 200) {
+      return FoodInfo.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load food info');
+    }
   }
 }
